8fe0cc1150ae9a00204a64cdd29fe70c463a8101
Dmitry Shevchenko <dmitry84@gmail.com>
2013-06-10 14:12:45 +0200

    Javascript based TOC creation

--- _assets/js/custom.js
+++ _assets/js/custom.js
@@ -1,3 +1,4 @@
+'use strict';
 $(document).ready(function() {
     /*show/hide moveTop link */
     var showMoveTop = false;
@@ -17,4 +18,33 @@ $(document).ready(function() {
             showMoveTop = false;
         }
     });
+
+    var ToC =
+        "<nav role='navigation' class='table-of-contents'>" +
+            "<h3>Table of contents:</h3>" +
+                "<ul>";
+
+    var newLine, el, title, link, elClass, url;
+    $("article h2, article h3, article h4").each(function() {
+        el      = $(this);
+        title   = el.text();
+        link    = "#" + el.attr("id");
+        elClass = "link_" + el.prop("tagName")
+        url     = window.location.pathname;
+
+
+        newLine =
+            "<li class='" + elClass  +  "'>" +
+                "<a href='" + url + link + "'>" +
+                    title +
+                "</a>" +
+            "</li>";
+
+        ToC += newLine;
+    });
+    ToC +=
+            "</ul>" +
+        "</nav>";
+
+    $("article #toc").append(ToC);
 });
