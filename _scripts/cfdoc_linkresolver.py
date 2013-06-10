0837306f40c0c1b1e53a90a8292ca51cb0f382bf
Volker Hilsheimer <volker.hilsheimer@cfengine.com>
2013-06-10 23:02:18 +0200

    Revert "No reason to replace space with - in key"
    This reverts commit 4dd4293c71269a98606729799b9c9d2a4ade7e01.

--- _scripts/cfdoc_linkresolver.py
+++ _scripts/cfdoc_linkresolver.py
@@ -47,13 +47,17 @@ def addToLinkFile(file_name,output_file,cur_dir):
 	for line in lines:
 		if line.find("title:") == 0:
 			current_title = line.split('title: ')					
-			current_title = current_title[1].rstrip()
-            current_title = current_title.lstrip()
+			current_title = current_title[1].rstrip()	
 		elif line.find("alias:") == 0:
-            current_file_name = line.split('alias: ')
+	
+			current_file_name = line.split('alias: ')					
 			current_file_name = current_file_name[1].rstrip()		
+			current_file_label = current_file_name.replace(".html","")				
+			current_file_label = "".join([ch for ch in current_file_label if ch in (ascii_letters) or ch == "-"])
+			current_file_label = current_file_label.lstrip('-')
 
-    current_file_label = current_title
+	current_file_label = current_title.replace(" ","-")	
+	current_file_label = current_file_label.lstrip('-')
 	output_string = '['+current_file_label+']: '+current_file_name+' \"'+current_title+'\"'
 
 	open(output_file, "a").write(output_string+"\n")
