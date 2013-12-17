245f332f53c4f45b9739d69c868588f82915692c
Volker Hilsheimer <volker.hilsheimer@cfengine.com>
2013-12-17 21:12:59 +0100

    Evaluate functions when validating partial promises.

--- _regenerate_json.sh
+++ _regenerate_json.sh
@@ -5,6 +5,6 @@ for in in ../masterfiles/lib/3.6/*.cf
 do
   out="${in/..\/masterfiles/_json}"
   out="${out/%.cf/.json}"
-  ../core/cf-promises/cf-promises --policy-output-format=json $in > $out
+  ../core/cf-promises/cf-promises --eval-functions --policy-output-format=json $in > $out
 done
 
