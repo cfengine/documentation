c31a977ff800828f888d2e6420a62d18e2706258
Volker Hilsheimer <volker.hilsheimer@cfengine.com>
2014-07-15 14:17:34 +0200

    Print log messages to stdout for easier diagnostics.

--- _scripts/cfdoc_qa.py
+++ _scripts/cfdoc_qa.py
@@ -81,10 +81,10 @@ def Log(config, string):
 		line_offset += 1
 		if line == "# Logging\n":
 			break
-			break
 
 	logfile.write("\n* %s\n" % string)
 	logfile.write("    * Triggered by: `%s` (%d)\n" % (os.path.relpath(config["context_current_file"]), config["context_current_line_number"]))
+	print "%s (%d): %s" % (os.path.relpath(config["context_current_file"]), config["context_current_line_number"], string)
 
 	logfile.writelines(original[line_offset:])
 	logfile.close()
