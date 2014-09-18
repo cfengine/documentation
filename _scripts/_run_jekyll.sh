c65165b24fcb8901aa2247e7fde08bf5c71a7311
Volker Hilsheimer <volker.hilsheimer@cfengine.com>
2014-09-18 13:56:52 +0200

    Remove automatic updating of json etc files

--- _scripts/_run_jekyll.sh
+++ _scripts/_run_jekyll.sh
@@ -1,47 +1,5 @@
 #!/bin/bash
-
-## /home/vagrant/_json is updated via MP API test run.
-## if content changed, make commits and create pull request
-## do not update working copies of generator in $WRKDIR
-## - only once pull request is merged
-
-cd $HOME/documentation-generator
-git fetch original
-git reset --hard original/master
-git clean -dfx
-cp -r $HOME/_json $HOME/documentation-generator
-cp $HOME/doc_help/* $HOME/documentation-generator/_generated
-cd $HOME/documentation-generator
-DIFF=$(git diff -- .)
-
-cd _json
-SYNTAX_DIFF=$(git diff -- syntax_map.json)
-if [ ! -z "$SYNTAX_DIFF" ]; then
-   git add syntax_map.json
-   git commit -m "Autocommit: syntax map changed `date +%F-%T`"
-fi
-cd ..
-
-cd _json/lib
-LIB_DIFF=$(git diff -- .)
-if [ ! -z "$LIB_DIFF" ]; then
-   git add -A -- .
-   git commit -m "Autocommit: libraries changed `date +%F-%T`"
-fi
-
-cd $HOME/documentation-generator/_generated
-HELP_DIFF=$(git diff -- .)
-if [ ! -z "$HELP_DIFF" ]; then
-   git add -A -- .
-   git commit -m "Autocommit: help text changed `date +%F-%T`"
-fi
-
-if [ ! -z "$DIFF" ]; then
-   git push -f
-   $HOME/bin/hub pull-request "Auto Pull Request" -b cfengine:master -h cfengine-autobuild:autocheckSyntaxMap
-fi
-cd $HOME/documentation-generator
-
+echo "Generating documentation from '$WRKDIR'..."
 
 sed '/^\[.*\[.*\].*\]/d' $WRKDIR/documentation-generator/_references.md > $WRKDIR/documentation-generator/new_references.md
 mv $WRKDIR/documentation-generator/new_references.md $WRKDIR/documentation-generator/_references.md
@@ -52,7 +10,7 @@ rm -rf $WRKDIR/documentation/.git
 rm -rf $WRKDIR/core/.git
 cp -r $WRKDIR/documentation/* $WRKDIR/documentation-generator/pages
 cd $WRKDIR/documentation-generator
-source /home/vagrant/.rvm/scripts/rvm
+source /home/jenkins/.rvm/scripts/rvm
 echo "Latest jekyll run :$BUILD_ID" > $WRKDIR/output.log
 echo "Based on latest git commit :$GIT_COMMIT" >> $WRKDIR/output.log
 echo "*********************************************************" >> $WRKDIR/output.log
