0051c6b532f83f8994ca9ab6b1900752574ea11b
Volker Hilsheimer <volker.hilsheimer@cfengine.com>
2014-09-22 15:46:27 +0200

    Generate help files as well, and simplify a bit

--- _regenerate_json.sh
+++ _regenerate_json.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
-mkdir -p $WRKDIR/documentation-generator/_generated/lib/3.7
-$WRKDIR/core/cf-promises/cf-promises -sjson > $WRKDIR/documentation-generator/_generated/syntax_map.json
+OUTDIR=$WRKDIR/documentation-generator/_generated
+mkdir -p $OUTDIR/lib/3.7
+$WRKDIR/core/cf-promises/cf-promises -sjson > $OUTDIR/syntax_map.json
 
 for lib in $WRKDIR/masterfiles/lib/3.7/*.cf
 do
-  out="${lib/$WRKDIR\/masterfiles/$WRKDIR/documentation-generator/_generated}"
+  out="${lib/$WRKDIR\/masterfiles/$OUTDIR}"
   out="${out/%.cf/.json}"
   $WRKDIR/core/cf-promises/cf-promises --eval-functions --policy-output-format=json $lib > $out
+done
+
+(
+    echo cf-agent
+    echo cf-promises
+    echo cf-runagent
+    echo cf-serverd
+    echo cf-execd
+    echo cf-key
+    echo cf-monitord
+) | while read agent
+do
+    $WRKDIR/core/$agent/$agent --help > $OUTDIR/$agent.help
 done
 
