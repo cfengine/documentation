874575073d027e021529ccdc13f42ee07a0b8c1c
Volker Hilsheimer <volker.hilsheimer@cfengine.com>
2014-09-22 18:09:14 +0200

    Move publishing steps into a script

---
+++ _scripts/_publish.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+VERSION=$1
+SERVER=$2
+PORT=$3
+
+cd $WRKDIR
+find documentation-generator/pages -name "*.markdown" | xargs rm
+cp `find documentation-generator/pages -name "*.*"` documentation-generator/_site
+if [  ! -d documentation-generator/_site ]; then
+  exit 1
+fi
+
+rsync -zvr -e "ssh -p $PORT" documentation-generator/_site/ $SERVER/$VERSION 
+
+ARCHIVE_FILE=cfengine-documentation-$VERSION
+echo "Creating $ARCHIVE_FILE..."
+cd documentation-generator/_site
+for source in *.html;
+do
+  sed -i "s/<base href\(.*\)>/<!-- base href\1 -->/" $source
+  sed -i "s/<link href='.*assets\(.*\)>/<link href='assets\1>/" $source
+  sed -i "s/<script src='.*assets\(.*\)>/<script src='assets\1>/" $source
+  sed -i "s/<select/<!-- select/" $source
+  sed -i "s/<\/select>/<\/select -->/" $source
+  sed -i "s/<form/<!-- select/" $source
+  sed -i "s/<\/form>/<\/form -->/" $source
+done
+cd ..
+tar -czf $ARCHIVE_FILE.tar.gz _site
+scp -P $PORT $ARCHIVE_FILE.tar.gz $SERVER/$VERSION 
