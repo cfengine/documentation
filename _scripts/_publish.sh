b8387a7ba0ffff7cc40a4b1ba640e319e08ae8c6
Nick Anderson <nick@cmdln.org>
2017-07-18 13:12:24 -0500

    Dont pack the site for shipping
    not working

--- _scripts/_publish.sh
+++ _scripts/_publish.sh
@@ -42,4 +42,4 @@ tar -czf $OUTPUT/$ARCHIVE_FILE.tar.gz _site
 mv $OUTPUT/$ARCHIVE_FILE.tar.gz _site/
 # Pack the site for transfer (its faster to transfer one big file than thousands of small ones)
 # This archive is expected to be unpacked by the build system after the artifacts have been moved to their storage location
-tar -czvf $OUTPUT/packed-for-shipping.tar.gz _site/ && rm -rf _site/
+# tar -czvf $OUTPUT/packed-for-shipping.tar.gz _site/ && rm -rf _site/
