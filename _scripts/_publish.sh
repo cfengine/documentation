23b9fdc4a17e4e047081a63b2b891ec91882ce8a
Nick Anderson <nick@cmdln.org>
2017-07-18 16:43:08 -0500

    Update _publish.sh

--- _scripts/_publish.sh
+++ _scripts/_publish.sh
@@ -40,6 +40,7 @@ cd ..
 tar -czf $OUTPUT/$ARCHIVE_FILE.tar.gz _site
 # Placing offline copy of docs into site directory
 mv $OUTPUT/$ARCHIVE_FILE.tar.gz _site/
+ls -al _site/$ARCHIVE_FILE.tar.gz
 # Pack the site for transfer (its faster to transfer one big file than thousands of small ones)
 # This archive is expected to be unpacked by the build system after the artifacts have been moved to their storage location
 # tar -czvf $OUTPUT/packed-for-shipping.tar.gz _site/ && rm -rf _site/
