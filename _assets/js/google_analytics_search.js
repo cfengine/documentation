09e657083b391556d401019559b3fbcce61a0649
Nick Anderson <nick@cmdln.org>
2019-09-19 09:23:41 -0500

    Added Google Custom Search Engine for 3.15

--- _assets/js/google_analytics_search.js
+++ _assets/js/google_analytics_search.js
@@ -1,7 +1,6 @@
 //google search
 (function() {
-    
-    var cx = '012826289311752768906:ofqwltf6-kg';
+    var cx = '012826289311752768906:zdz4ud6t2xf';
     var gcse = document.createElement('script');
     gcse.type = 'text/javascript';
     gcse.async = true;
