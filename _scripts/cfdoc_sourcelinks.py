9938e7e0cb01b3ee3021fb851e2ed5ceba3c3873
Nick Anderson <nick@cmdln.org>
2021-09-21 09:24:46 -0500

    Whitespace cleanup

--- _scripts/cfdoc_sourcelinks.py
+++ _scripts/cfdoc_sourcelinks.py
@@ -24,111 +24,111 @@ import os
 import re
 
 def run(config):
-	verifyLinkRegex(unresolvedLinkRegex())
-	verifyMacroRegex(unexpandedMacroRegex())
+    verifyLinkRegex(unresolvedLinkRegex())
+    verifyMacroRegex(unexpandedMacroRegex())
 
-	markdown_files = config["markdown_files"]
-	error_count = 0
-	for file in markdown_files:
-		error_count += addLinkToSource(file, config)
-	if error_count:
-		raise Exception("%d errors while processing HTML files" % error_count)
+    markdown_files = config["markdown_files"]
+    error_count = 0
+    for file in markdown_files:
+        error_count += addLinkToSource(file, config)
+    if error_count:
+        raise Exception("%d errors while processing HTML files" % error_count)
 
 def verifyRegex(regex, tests):
-	for test in tests:
-		line = test[0]
-		expected = test[1]
-		actual = regex.search(line) != None
-		if actual != expected:
-			print "Programming error: regex '%s' doesn't match '%s' correctly!" % (regex, line)
-			print "    expected result: %s" % expected
-			exit(-1)
+    for test in tests:
+        line = test[0]
+        expected = test[1]
+        actual = regex.search(line) != None
+        if actual != expected:
+            print "Programming error: regex '%s' doesn't match '%s' correctly!" % (regex, line)
+            print "    expected result: %s" % expected
+            exit(-1)
 
 def unresolvedLinkRegex():
-	# should include `:` behind closing bracket, but leads to false positives
-	return re.compile("(^|\\s+|>)\\[.+?\\]\\[.*?\\](\\s|[,\\.;,]|$|</li>|</ul>)")
+    # should include `:` behind closing bracket, but leads to false positives
+    return re.compile("(^|\\s+|>)\\[.+?\\]\\[.*?\\](\\s|[,\\.;,]|$|</li>|</ul>)")
 
 def verifyLinkRegex(regex):
-	tests = []
-	# uresolved links
-	tests.append(("[a][b]\n", True))
-	tests.append(("text [a][b] more text", True))
-	tests.append(("text [a][b].", True))
-	tests.append(("text [a][b],", True))
-	tests.append(("text [a][b];", True))
-	tests.append(("text [a][b]:", False)) # KNOWN ISSUE
-	tests.append(("text [a][]", True)) # shorthand
-	tests.append(("<li>[a][b]\n", True)) # bullet list
-	tests.append(("<li>[a][b]</li>", True)) # bullet list
-	tests.append(("text [a][b]", True)) # missing lineend and eof
-	
-	# ok
-	tests.append(("a[a][b]\n", False)) # Array
-	tests.append(("[][]\n", False)) # Not markdown
-	tests.append(("[][a]\n", False)) # Not markdown
-	tests.append(("[a][b][c]\n", True)) # enumeration - KNOWN ISSUE
-	tests.append(("[a][b]text\n", False)) # Something else
-	tests.append(("[a][b]</span>", False)) # code stuff
-	
-	verifyRegex(regex, tests)
+    tests = []
+    # uresolved links
+    tests.append(("[a][b]\n", True))
+    tests.append(("text [a][b] more text", True))
+    tests.append(("text [a][b].", True))
+    tests.append(("text [a][b],", True))
+    tests.append(("text [a][b];", True))
+    tests.append(("text [a][b]:", False)) # KNOWN ISSUE
+    tests.append(("text [a][]", True)) # shorthand
+    tests.append(("<li>[a][b]\n", True)) # bullet list
+    tests.append(("<li>[a][b]</li>", True)) # bullet list
+    tests.append(("text [a][b]", True)) # missing lineend and eof
+
+    # ok
+    tests.append(("a[a][b]\n", False)) # Array
+    tests.append(("[][]\n", False)) # Not markdown
+    tests.append(("[][a]\n", False)) # Not markdown
+    tests.append(("[a][b][c]\n", True)) # enumeration - KNOWN ISSUE
+    tests.append(("[a][b]text\n", False)) # Something else
+    tests.append(("[a][b]</span>", False)) # code stuff
+
+    verifyRegex(regex, tests)
 
 def unexpandedMacroRegex():
-	return re.compile("\\[%CFEngine_.*%\\]")
-	
-def verifyMacroRegex(regex):
-	tests = []
-	# unexpanded macro
-	tests.append(("[%CFEngine_include_snippet(foo)%]\n", True))
-	tests.append(("[%CFEngine_include_snippet()%]\n", True))
-	
-	# ok
-	tests.append(("[%CFTemplate%]\n", False))
+    return re.compile("\\[%CFEngine_.*%\\]")
 
-	verifyRegex(regex, tests)
+def verifyMacroRegex(regex):
+    tests = []
+    # unexpanded macro
+    tests.append(("[%CFEngine_include_snippet(foo)%]\n", True))
+    tests.append(("[%CFEngine_include_snippet()%]\n", True))
+
+    # ok
+    tests.append(("[%CFTemplate%]\n", False))
+
+    verifyRegex(regex, tests)
 
 def addLinkToSource(file_name,config):
-	in_file = open(file_name,"r")
-	lines = in_file.readlines()
-	in_file.close()
-	source_file = file_name[config["markdown_directory"].__len__():]
-	html_file = ""
+    in_file = open(file_name,"r")
+    lines = in_file.readlines()
+    in_file.close()
+    source_file = file_name[config["markdown_directory"].__len__():]
+    html_file = ""
 
-	for line in lines:
-		if line.find("alias:") == 0:
-			html_file = line.split('alias: ')
-			html_file = html_file[1].rstrip()
-			break
-		if line.find("layout: printable") == 0:
-			return 0
-	
-	if not html_file:
-		return 0
-	
-	unresolved_link = unresolvedLinkRegex()
-	unexpanded_macro = unexpandedMacroRegex()
+    for line in lines:
+        if line.find("alias:") == 0:
+            html_file = line.split('alias: ')
+            html_file = html_file[1].rstrip()
+            break
+        if line.find("layout: printable") == 0:
+            return 0
 
-	error_count = 0
-	html_file = config['CFE_DIR'] + "/" + html_file
-	try:
-		in_file = open(html_file, "r")
-		lines = in_file.readlines()
-		in_file.close()
-	except:
-		print "cfdoc_sourcelinks: Error opening " + html_file
-		return 0 # tolerate missing HTML files
+    if not html_file:
+        return 0
 
-	new_html_file = html_file + ".new"
-	out_file = open(new_html_file, "w")
-	for line in lines:
-		line = line.replace("\">markdown source</a>]", source_file + "\">markdown source</a>]")
-		if unresolved_link.search(line) != None:
-			print "Unresolved link in '%s', html-line '%s'\n" % (file_name, line)
-			error_count += 1
-		if unexpanded_macro.search(line) != None:
-			print "Unexpanded macro in '%s', html-line '%s'\n" % (file_name, line)
-			error_count += 1
-		out_file.write(line)
-	out_file.close()
-	
-	os.rename(new_html_file,html_file)
-	return error_count
+    unresolved_link = unresolvedLinkRegex()
+    unexpanded_macro = unexpandedMacroRegex()
+
+    error_count = 0
+    html_file = config['CFE_DIR'] + "/" + html_file
+    try:
+        in_file = open(html_file, "r")
+        lines = in_file.readlines()
+        in_file.close()
+    except:
+        print "cfdoc_sourcelinks: Error opening " + html_file
+        return 0 # tolerate missing HTML files
+
+    new_html_file = html_file + ".new"
+    out_file = open(new_html_file, "w")
+    for line in lines:
+        line = line.replace("\">markdown source</a>]", source_file + "\">markdown source</a>]")
+        if unresolved_link.search(line) != None:
+            print "Unresolved link in '%s', html-line '%s'\n" % (file_name, line)
+            error_count += 1
+        if unexpanded_macro.search(line) != None:
+            print "Unexpanded macro in '%s', html-line '%s'\n" % (file_name, line)
+            error_count += 1
+        out_file.write(line)
+    out_file.close()
+
+    os.rename(new_html_file,html_file)
+    return error_count
