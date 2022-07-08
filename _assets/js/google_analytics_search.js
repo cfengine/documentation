0e6b1c2dbfa383ec6553430116f5243dabe468f6
Nick Anderson <nick@cmdln.org>
2022-07-08 07:07:20 -0500

    Added custom search for 3.20 docs
    Ticket: ENT-8635
    Changelog: None

--- _assets/js/google_analytics_search.js
+++ _assets/js/google_analytics_search.js
@@ -1,7 +1,7 @@
 //google search
 (function() {
     
-    var cx = '012826289311752768906:ofqwltf6-kg';
+    var cx = '96c0f638b03564284';
     var gcse = document.createElement('script');
     gcse.type = 'text/javascript';
     gcse.async = true;
