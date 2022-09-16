9078b6ef5b263bd51bb4ee25309c74e5bbe7c8f3
Aleksei Shpakovskii <aleksei.shpakovskii@northern.tech>
2022-09-16 18:34:43 +0200

    debug output

--- generator/build/run.sh
+++ generator/build/run.sh
@@ -24,7 +24,8 @@ sudo chmod -R a+rwX $PWD
 c=$(buildah from -v $PWD:/nt docs-revamp-22)
 trap "buildah rm $c >/dev/null; sudo chmod -R a+rwX $PWD" EXIT
 ls -lah documentation/generator/build/main.sh
-buildah run $c ls -lah documentation/generator/build/main.sh
+buildah run $c ls -lah documentation
+buildah run $c ls -lah documentation/generator/build
 buildah run $c bash -x documentation/generator/build/main.sh $BRANCH $PACKAGE_JOB $PACKAGE_UPLOAD_DIRECTORY $PACKAGE_BUILD
 buildah run $c bash -x documentation/generator/_scripts/_publish.sh $BRANCH
 
