8f6e1c00a6694a88e6fc97e4ec1985c63c7a50f7
Craig Comstock <craig.comstock@northern.tech>
2023-08-17 15:47:35 -0500

    Fixed host ownership after container builds
    Inside of the container build we chown all files to jenkins:jenkins but this results
    in the host having incorrect ownership after the run which then causes problems
    when CI comes back again to checkout with git.
    
    Fix is to chown back to the local uid/gid regardless of outcome of run.sh (the build).
    
    Ticket: ENT-10474
    Changelog: none

--- generator/build/run.sh
+++ generator/build/run.sh
@@ -21,6 +21,6 @@ true "${PACKAGE_UPLOAD_DIRECTORY?undefined}"
 true "${PACKAGE_BUILD?undefined}"
 
 c=$(buildah from -v $PWD:/nt docs-revamp-22)
-trap "buildah run $c bash -c 'sudo chmod -R a+rwX /nt'; buildah rm $c >/dev/null" EXIT
+trap "buildah run $c bash -c 'sudo chown -R root:root /nt; sudo chmod -R a+rwX /nt'; buildah rm $c >/dev/null" EXIT
 buildah run $c bash -x documentation/generator/build/main.sh $BRANCH $PACKAGE_JOB $PACKAGE_UPLOAD_DIRECTORY $PACKAGE_BUILD
 buildah run $c bash -x documentation/generator/_scripts/_publish.sh $BRANCH
