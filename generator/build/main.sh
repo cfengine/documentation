8d914a320c3a1e711a026f656be9eae20d10381a
Ihor Aleksandrychiev <ihor.aleksandrychiev@northern.tech>
2025-05-29 15:16:14 +0300

    adjusted scripts

--- generator/build/main.sh
+++ generator/build/main.sh
@@ -104,25 +104,10 @@ echo "branch: $BRANCH" >> "$WRKDIR"/documentation/generator/_config.yml
 # so instead of set -x we just echo each command ourselves
 set +x
 
-# since May 14 2019, we need this to run jekyll. IDK why.
-echo "+ source ~/.rvm/scripts/rvm"
-# shellcheck disable=SC1090
-source ~/.rvm/scripts/rvm
-echo "+ rvm --default use 1.9.3-p551"
-rvm --default use 1.9.3-p551
-echo "+ source ~/.profile"
-ls -lah ~
-# shellcheck disable=SC1090
-test -f ~/.profile && source ~/.profile
-echo "+ source ~/.rvm/scripts/rvm"
-# shellcheck disable=SC1090
-source ~/.rvm/scripts/rvm
-
 export LC_ALL=C.UTF-8
 
-# finally, run actual jekyll
-echo "+ bash -x ./_scripts/_run_jekyll.sh $BRANCH || exit 6"
-bash -x ./_scripts/_run_jekyll.sh "$BRANCH" || exit 6
+echo "+ bash -x ./_scripts/_run.sh $BRANCH || exit 6"
+bash -x ./_scripts/_run.sh "$BRANCH" || exit 6
 
 cd "$WRKDIR"/documentation/generator
 npm run build
