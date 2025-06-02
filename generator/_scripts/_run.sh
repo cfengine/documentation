5024581eb992f320294791d6c0c66aa3936427fd
Ihor Aleksandrychiev <ihor.aleksandrychiev@northern.tech>
2025-06-02 13:25:22 +0300

    fixed content cp
    Signed-off-by: Ihor Aleksandrychiev <ihor.aleksandrychiev@northern.tech>

--- generator/_scripts/_run.sh
+++ generator/_scripts/_run.sh
@@ -14,13 +14,14 @@ mv $WRKDIR/documentation/generator/new_references.md $WRKDIR/documentation/gener
 
 mkdir $WRKDIR/documentation/generator/pages
 cp -r $WRKDIR/documentation/content/* $WRKDIR/documentation/generator/pages
-cp -rf $WRKDIR/documentation/content/* $WRKDIR/documentation/hugo/content
 cp -r $WRKDIR/documentation/generator/_includes/lts_versions_list.html $WRKDIR/documentation/hugo/static/
 cp -rf $WRKDIR/documentation/generator/_includes/versions_list.html $WRKDIR/documentation/hugo/layouts/partials/
 cp -rf $WRKDIR/documentation/generator/_includes/header_nav_options.html $WRKDIR/documentation/hugo/layouts/partials/
 rm -rf $WRKDIR/documentation/generator/pages/generator
 # remove not published .markdown files
 find $WRKDIR/documentation/generator/pages -type f -name "*.markdown" -exec grep -l '^published: false$' {} + | xargs rm -f
+mkdir $WRKDIR/documentation/hugo/content
+cp -rf $WRKDIR/documentation/generator/pages/* $WRKDIR/documentation/hugo/content
 
 # Hugo build
 cd $WRKDIR/documentation/hugo
@@ -30,7 +31,6 @@ npm ci
 npm run build:all
 
 cd $WRKDIR/documentation/generator
-mkdir $WRKDIR/documentation/generator/_site/assets/
 cp -rf $WRKDIR/documentation/hugo/scripts/search/index/searchIndex $WRKDIR/documentation/generator/_site/assets/
 
 $WRKDIR/documentation/generator/_scripts/cfdoc_postprocess.py "$@"
