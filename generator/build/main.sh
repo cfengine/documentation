1592f2a70fb2303136fcade34f74b8224dd71603
Craig Comstock <craig.comstock@northern.tech>
2025-08-13 15:02:09 -0500

    cf-remote fixup

--- generator/build/main.sh
+++ generator/build/main.sh
@@ -77,8 +77,8 @@ test ! -z "$PACKAGE_BUILD"
 echo "Install hub package"
 if [ "$PACKAGE_JOB" = "cf-remote" ]; then
   echo "Install using cf-remote"
-  apt update -y
-  apt install -y python3-venv pipx
+  sudo apt update -y
+  sudo apt install -y python3-venv pipx
   pipx install cf-remote
   cf-remote --version "$BRANCH" install --hub localhost
 else
