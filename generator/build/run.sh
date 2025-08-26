c2a02f7cf4893c89e23d39fbb1d441cf7d3433bd
Craig Comstock <craig.comstock@northern.tech>
2025-08-26 12:28:15 -0500

    save an install.sh.log for debugging and refactor image name for easier versioning

--- generator/build/run.sh
+++ generator/build/run.sh
@@ -3,8 +3,9 @@
 set -ex
 trap "echo FAILURE" ERR
 
-if ! buildah inspect docs-hugo >/dev/null 2>&1; then
-  buildah build-using-dockerfile -t docs-hugo documentation/generator/build
+image_name=docs-hugo-v0
+if ! buildah inspect "$image_name" >/dev/null 2>&1; then
+  buildah build-using-dockerfile -t "$image_name" documentation/generator/build
 fi
 
 # Current path must have the following repos cloned:
@@ -30,7 +31,7 @@ elif [ -n "$BRANCH_NAME" ]; then
   BRANCH="$BRANCH_NAME"
 fi
 
-c=$(buildah from -v "$PWD":/nt docs-hugo)
+c=$(buildah from -v "$PWD":/nt "$image_name")
 trap 'buildah run "$c" bash -c "sudo chown -R root:root /nt; sudo chmod -R a+rwX /nt"; buildah rm "$c" >/dev/null' EXIT
 buildah run "$c" bash -x documentation/generator/build/main.sh "$BRANCH" "$PACKAGE_JOB" "$PACKAGE_UPLOAD_DIRECTORY" "$PACKAGE_BUILD"
 buildah run "$c" bash -x documentation/generator/_scripts/_publish.sh "$BRANCH"
