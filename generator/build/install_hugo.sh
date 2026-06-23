5baae57b9b1277b5b05ad65dee60cfce14eca4cc
Ihor Aleksandrychiev <ihor.aleksandrychiev@northern.tech>
2026-06-23 15:02:16 +0300

    Download hugo in the non-verbose mode
    Signed-off-by: Ihor Aleksandrychiev <ihor.aleksandrychiev@northern.tech>

--- generator/build/install_hugo.sh
+++ generator/build/install_hugo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget https://github.com/gohugoio/hugo/releases/download/v0.163.3/hugo_0.163.3_Linux-64bit.tar.gz -O hugo.tar.gz
+wget -nv https://github.com/gohugoio/hugo/releases/download/v0.163.3/hugo_0.163.3_Linux-64bit.tar.gz -O hugo.tar.gz
  echo "ec422258f9a4ffc241de8707297e32311cd86fcc9b2813632617ff4d44935d91  hugo.tar.gz" | sha256sum -c
 if [ $? -eq 1 ]; then
   exit 2
